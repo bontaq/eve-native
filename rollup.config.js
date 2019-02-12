85bab8a10931091b796c9b5acca6d34bae7027f4
Ian Davidson <ian.davidson@onepeloton.com>
2019-02-12 17:31:18 -0500

    fix up some things

--- rollup.config.js
+++ rollup.config.js
@@ -1,46 +1,52 @@
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import typescript from "rollup-plugin-typescript2";
-import json from "rollup-plugin-json";
-import multiEntry from "rollup-plugin-multi-entry";
-import postcss from "rollup-plugin-postcss";
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+import typescript from 'rollup-plugin-typescript2';
+import json from 'rollup-plugin-json';
+import multiEntry from 'rollup-plugin-multi-entry';
+import postcss from 'rollup-plugin-postcss';
 
 // Post CSS plugins
-import autoprefixer from "autoprefixer";
-import customProperties from "postcss-custom-properties";
-import colorFunction from "postcss-color-function";
+import autoprefixer from 'autoprefixer';
+import customProperties from 'postcss-custom-properties';
+import colorFunction from 'postcss-color-function';
 
 function createBundle(name, entry, moduleName) {
-  if(moduleName === undefined) moduleName = name;
+  if (moduleName === undefined) moduleName = name;
   return {
     moduleName,
     entry,
     dest: `./dist/${name}.js`,
 
-    format: "iife",
+    format: 'iife',
+    experimentalCodeSplitting: true,
     plugins: [
-      multiEntry(),
-      resolve({main: true, browser: true}),
       commonjs({
         namedExports: {
-          "uuid": ["v4"]
-        }
+          uuid: ['v4'],
+        },
       }),
+      multiEntry(),
+      resolve({ main: true, browser: true }),
       typescript({}),
       json(),
       postcss({
         extract: `./dist/${name}.css`,
-        plugins: [
-          autoprefixer(),
-          customProperties(),
-          colorFunction()
-        ]
+          plugins: [autoprefixer(), customProperties(), colorFunction()],
+          extensions: [ '.css' ],
       }),
     ],
-  }
+  };
 }
 
 export default [
-  createBundle("eve-libraries", ["./ts/main.ts", "./libraries/**/*.css"], "eve_libraries"),
-  createBundle("programs/editor", ["./examples/editor/**/*.css"], "eve_programs_editor")
+  createBundle(
+    'eve-libraries',
+    ['./ts/main.ts'],
+    'eve_libraries',
+  ),
+//  createBundle(
+//    'programs/editor',
+//    ['./examples/editor/**/*.css'],
+//    'eve_programs_editor',
+//  ),
 ];
--- ts/connection.ts
+++ ts/connection.ts
@@ -33,7 +33,9 @@ export class Connection {
       // @NOTE: this doesn't gracefully handle partial processing of the queue.
       while(this._queue.length) {
         let payload = this._queue.shift();
-        this.ws.send(payload);
+        if (payload) {
+          this.ws.send(payload);
+        }
       }
     }
   }
